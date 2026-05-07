1531427ddb89190a50f3c8f80ca696cc7ba2779e
v4ler1e <Valeria.Motorenko@tptlive.ee>
2026-05-07 14:22:08 +0300

    Add rode module exammple (fs)

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello via Bun!");
+
+//const common = require('./common.js');
+import common from './common.js';
+common.hello();
+
+import es from './es.js';
+es.hello();
+
+import fs from 'fs';
+fs.writeFileSync('test.txt', 'Hello, file!');
