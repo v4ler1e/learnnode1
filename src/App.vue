bdf02104fb71cef9b0fd8410604deca507a00e38
v4ler1e <Valeria.Motorenko@tptlive.ee>
2026-05-22 13:51:05 +0300

    Add reverse text example

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
-    let message = 'Hello Vue!';
+import { ref } from 'vue';
+
+let message = ref('Hello Vue!');
+
+
 </script>
 
 <template>
-    <h1>{{ message }}</h1>
+    <h1>{{ message.split('').reverse().join('') }}</h1>
+    <button class="button is-primary" @click="message = 'Tello TA24B!'">Click me!</button>
+    <input class="input" v-model="message">
 </template>
 
 <style>
