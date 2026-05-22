8b34f85be715a8243f54e1d6b9bd106adebf6cae
v4ler1e <Valeria.Motorenko@tptlive.ee>
2026-05-22 14:28:49 +0300

    Add todo list

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <script setup>
 import { ref } from 'vue';
+let items = ref(['Sai', 'Piim', 'Viin' , 'Beer', 'Tee']);
+let newItem = ref('');
 
-let message = ref('Hello Vue!');
-
-
+function add() {
+    if(newItem.value.trim() !== '') {
+    items.value.push(newItem.value.trim());
+    }
+    newItem.value = '';
+}
 </script>
 
 <template>
-    <h1>{{ message.split('').reverse().join('') }}</h1>
-    <button class="button is-primary" @click="message = 'Tello TA24B!'">Click me!</button>
-    <input class="input" v-model="message">
+    <div class="container">
+        <div class="field has-addons mt-2">
+            <div class="control is-expanded">
+                <input v-model="newItem" class="input" type="text" @keypress.enter="add">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="add">
+                    Add Item
+                </button>
+            </div>
+        </div>
+            <div class="content">  
+                <h1>All Items</h1>
+                    <ul>
+                        <li v-for="item in items">{{ item }}</li>
+                    </ul>
+            </div>
+    </div>
 </template>
 
 <style>
