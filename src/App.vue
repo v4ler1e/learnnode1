6bc3ade117811397eb6681720b99f98633f9be72
v4ler1e <Valeria.Motorenko@tptlive.ee>
2026-09-14 21:35:30 +0300

    Add itemlist component

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,47 @@
 <script setup>
-import { computed , ref } from 'vue';
+import { computed, ref } from "vue";
 let i = 1;
 
 let items = ref([
-    {id: i++, text: 'Sai', isDone: false},
-    {id: i++, text: 'Piim', isDone: true},
-    {id: i++, text: 'Viin', isDone: false},
-    {id: i++, text: 'Beer', isDone: true},
+  { id: i++, text: "Sai", isDone: false },
+  { id: i++, text: "Piim", isDone: true },
+  { id: i++, text: "Viin", isDone: false },
+  { id: i++, text: "Beer", isDone: true },
 ]);
-let newItem = ref('');
+let newItem = ref("");
 
-let doneItems = computed(() => items.value.filter(item => item.isDone));
-let todoItems = computed(() => items.value.filter(item => !item.isDone));
+let doneItems = computed(() => items.value.filter((item) => item.isDone));
+let todoItems = computed(() => items.value.filter((item) => !item.isDone));
 
 function add() {
-    if(newItem.value.trim() !== '') {
-    items.value.push({id: i++, text: newItem.value.trim(), isDone: false});
-    }
-    newItem.value = '';
+  if (newItem.value.trim() !== "") {
+    items.value.push({ id: i++, text: newItem.value.trim(), isDone: false });
+  }
+  newItem.value = "";
 }
 </script>
 
 <template>
-    <div class="container">
-        <div class="field has-addons mt-2">
-            <div class="control is-expanded">
-                <input v-model="newItem" class="input" type="text" @keypress.enter="add">
-            </div>
-            <div class="control">
-                <button class="button is-info" @click="add">
-                    Add Item
-                </button>
-            </div>
-        </div>
-            <div class="content">  
-                <h1>All Items</h1>
-                    <ul>
-                        <li v-for="item in items" :key="item.id">
-                            {{ item.text }}
-                            <input type="checkbox" v-model="item.isDone">
-                        </li>
-                    </ul>
-
-                    <h1>Done Items</h1>
-                    <ul>
-                        <li v-for="item in doneItems" :key="item.id">
-                            {{ item.text }}
-                            <input type="checkbox" v-model="item.isDone">
-                        </li>
-                    </ul>
-
-                    <h1>ToDo Items</h1>
-                    <ul>
-                        <li v-for="item in todoItems" :key="item.id">
-                            {{ item.text }}
-                            <input type="checkbox" v-model="item.isDone">
-                        </li>
-                    </ul>
-            </div>
+  <div class="container">
+    <div class="field has-addons mt-2">
+      <div class="control is-expanded">
+        <input
+          v-model="newItem"
+          class="input"
+          type="text"
+          @keypress.enter="add"
+        />
+      </div>
+      <div class="control">
+        <button class="button is-info" @click="add">Add Item</button>
+      </div>
     </div>
+    <div class="content">
+      <ItemList :items="items" title="All Items"> </ItemList>
+      <ItemList :items="doneItems" title="Done Items"> </ItemList>
+      <ItemList :items="todoItems" title="To-Do Items"> </ItemList>
+    </div>
+  </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
