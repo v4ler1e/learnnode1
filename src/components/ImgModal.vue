4d9a310d17dc49fc96417b31a2775f5412c289bd
v4ler1e <Valeria.Motorenko@tptlive.ee>
2026-09-14 23:44:15 +0300

    Add Modal and ImgModal Examples

---
+++ src/components/ImgModal.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Modal from './Modal.vue';
+defineProps(["active" , "url"]);
+
+</script>
+<template>
+    <Modal :active="active">
+        <p class="image is 4by3">
+            <img :src="url" alt="">
+        </p>
+    </Modal>
+</template>
---
+++ src/components/Modal.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(["active"]);
+</script>
+<template>
+<div class="modal" :class="{ 'is-active': active }">
+    <div class="modal-background" @click="$emit('close') = false"></div>
+    <div class="modal-content">
+      
+      <slot></slot>
+    </div>
+    <button
+      class="modal-close is-large"
+      aria-label="close"
+      @click="$emit('close')"
+    ></button>
+</div>
+</template>
