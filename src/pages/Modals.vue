0cfb56dba2ac87a42c38b1321ed1eb17a2b52bb9
v4ler1e <Valeria.Motorenko@tptlive.ee>
2026-09-14 23:47:36 +0300

    Clean up example

---
+++ src/pages/Modals.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+import Modal from "./components/Modal.vue";
+import ImgModal from "./components/ImgModal.vue";
+
+let modal1Active = ref(false);
+let modal2Active = ref(false);
+</script>
+
+<template>
+  <div class="container">
+    <button class="button is-primary" @click="modal1Active = true">
+      Open Modal 1
+    </button>
+    <button class="button is-info" @click="modal2Active = true">
+      Open Modal 2
+    </button>
+  <Modal :active="modal1Active" @close="modal1Active = false">
+    <div class="box"> I'm in a box.</div>
+  </Modal>
+  <ImgModal :active="modal2Active" @close="modal2Active = false" url="https://picsum.photos/1280/960?r=2"></ImgModal>
+  </div>
+</template>
+
+<style></style>
