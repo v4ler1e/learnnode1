cd689c038fb445c82efc3d0353add0f1f77e7af6
v4ler1e <Valeria.Motorenko@tptlive.ee>
2026-09-14 21:49:07 +0300

    Restructure Project

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,7 @@
-<script setup>
-import { computed, ref } from "vue";
-let i = 1;
-
-let items = ref([
-  { id: i++, text: "Sai", isDone: false },
-  { id: i++, text: "Piim", isDone: true },
-  { id: i++, text: "Viin", isDone: false },
-  { id: i++, text: "Beer", isDone: true },
-]);
-let newItem = ref("");
-
-let doneItems = computed(() => items.value.filter((item) => item.isDone));
-let todoItems = computed(() => items.value.filter((item) => !item.isDone));
-
-function add() {
-  if (newItem.value.trim() !== "") {
-    items.value.push({ id: i++, text: newItem.value.trim(), isDone: false });
-  }
-  newItem.value = "";
-}
-</script>
+<script setup></script>
 
 <template>
-  <div class="container">
-    <div class="field has-addons mt-2">
-      <div class="control is-expanded">
-        <input
-          v-model="newItem"
-          class="input"
-          type="text"
-          @keypress.enter="add"
-        />
-      </div>
-      <div class="control">
-        <button class="button is-info" @click="add">Add Item</button>
-      </div>
-    </div>
-    <div class="content">
-      <ItemList :items="items" title="All Items"> </ItemList>
-      <ItemList :items="doneItems" title="Done Items"> </ItemList>
-      <ItemList :items="todoItems" title="To-Do Items"> </ItemList>
-    </div>
-  </div>
+  <div class="container"></div>
 </template>
 
 <style></style>
