7dab9c6e3e770b7c304a9d5a66056407cb109c80
v4ler1e <Valeria.Motorenko@tptlive.ee>
2026-09-15 00:39:56 +0300

    Add Tab components and examples

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script setup>
 import { ref } from "vue";
+import Tabs from "./components/Tabs.vue";
+
+let tabTitles = ref([
+  'Title 1',
+  'Title 2',
+  'Title 3',
+  'Title 4',
+])
+
+let tabActive = ref(0);
+
+let content = ref([
+  'Content 1',
+  'Content 2',
+  'Content 3',
+  'Content 4',
+])
 
 </script>
 
 <template>
   <div class="container">
-    
+    <Tabs :titles="tabTitles" :active="tabActive" @setActive="tabActive = $event"></Tabs>
+    <p> {{ content[tabActive] }} </p>
   </div>
 </template>
 
---
+++ src/components/Tabs.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps(["titles", "active"]);
+</script>
+<template>
+    <div class="tabs is-centered">
+        <ul>
+            <li v-for="(title, index) in titles" :class="{ 'is-active': index === active }"
+                @click="$emit('setActive', index)"><a>{{ title }}</a></li>
+
+        </ul>
+    </div>
+</template>
