06dc3df6374a34bdc20bfebd8557948916fe4b88
v4ler1e <Valeria.Motorenko@tptlive.ee>
2026-09-16 16:38:13 +0300

    fixed modal.vue

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,17 +1,16 @@
 <script setup>
 defineProps(["active"]);
+defineEmits(["close"]);
 </script>
+
 <template>
-<div class="modal" :class="{ 'is-active': active }">
-    <div class="modal-background" @click="$emit('close') = false"></div>
+  <div class="modal" :class="{ 'is-active': active }">
+    <div class="modal-background" @click="$emit('close')"></div>
+
     <div class="modal-content">
-      
       <slot></slot>
     </div>
-    <button
-      class="modal-close is-large"
-      aria-label="close"
-      @click="$emit('close')"
-    ></button>
-</div>
+
+    <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+  </div>
 </template>
